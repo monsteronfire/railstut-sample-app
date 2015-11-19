9134fa579e00b6d1da3506b575cc168b79aa4def
monsteronfire <monsteronfire@gmail.com>
2015-11-19 10:21:38 +0800

    adds login and login flash test

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,5 @@
 class SessionsController < ApplicationController
+
   def new
   end
 
@@ -7,8 +8,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       # Log the user in and redirect to the user's show page.
     else
-      # Create an error message.
-      flash[:danger] = 'Invalid email/password combination' # Not quite right!
+      flash.now[:danger] = 'Invalid email/password combination' # Not quite right!
       render 'new'
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -6,12 +6,12 @@ Rails.application.routes.draw do
   get 'users/new'
 
   root             'static_pages#home'
-  get 'help',    to:'static_pages#help'
-  get 'about',   to:'static_pages#about'
-  get 'contact', to:'static_pages#contact'
-  get 'signup',  to:'users#new'
-  get 'login',   to:'sessions#new'
-  get 'login',   to:'sessions#create'
-  get 'logout',  to:'sessions#destroy'
+  get    'help',    to:'static_pages#help'
+  get    'about',   to:'static_pages#about'
+  get    'contact', to:'static_pages#contact'
+  get    'signup',  to:'users#new'
+  get    'login',   to:'sessions#new'
+  post   'login',   to:'sessions#create'
+  delete 'logout',  to:'sessions#destroy'
 
 end
